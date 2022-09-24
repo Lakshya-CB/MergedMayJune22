20246b45fc3edaf4f0d0425b2c832bbce8c897ba
Lakshya Singh <78645656+Lakshya-CB@users.noreply.github.com>
2022-09-24 18:06:26 +0530

    Add files via upload

---
+++ Lec_23/Student.java
@@ -0,0 +1,16 @@
+package Lec_23;
+
+public class Student {
+	String Name;
+	int Age;
+
+	public void Intro() {
+//		System.out.println(this);
+//		System.out.println(this.Name + " " + this.Age);
+		System.out.println(Name + " " + Age);
+		
+	}
+	public void Slap(String Name) {
+		System.out.println(this.Name+" slaped "+Name);
+	}
+}
